c389645ab496b786adb9ee3472ec288b3f29990f
Patryk Jędrzejczak <patryk.jed123@gmail.com>
2022-03-26 22:32:21 +0100

    Created GameEngine class

---
+++ src/GameEngine.cpp
@@ -0,0 +1,22 @@
+#include "../include/GameEngine.h"
+
+GameEngine::GameEngine() {
+  sf::Vector2i resolution;
+  resolution.x = sf::VideoMode::getDesktopMode().width;
+  resolution.y = sf::VideoMode::getDesktopMode().height;
+
+  _window.create(sf::VideoMode(resolution.x, resolution.y), "Pacman Rivals", sf::Style::Fullscreen);
+}
+
+void GameEngine::run() {
+  sf::Clock clock;
+
+  while (_window.isOpen()) {
+    sf::Time dt = clock.restart();
+    float dt_as_seconds = dt.asSeconds();
+
+    input();
+    update(dt_as_seconds);
+    draw();
+  }
+}
