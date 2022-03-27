a6f4d475f330dbd86c1e4d425b748883604130f3
Patryk Jędrzejczak <patryk.jed123@gmail.com>
2022-03-27 23:01:55 +0200

    Created TextureHolder

---
+++ src/texture-holder/TextureHolder.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <map>
+
+// Singleton class that stores references of textures.
+class TextureHolder {
+
+private:
+
+  std::map<std::string, sf::Texture> _textures;
+
+  static TextureHolder* _instance;
+
+public:
+
+  TextureHolder();
+
+  static sf::Texture & GetTexture(const std::string &filename);
+
+};
