3d0990c0ab68aab911deb7590e94650727bdf9e0
jullia <julia.sobiech@gmail.com>
2022-03-29 00:06:58 +0200

    delete unused structures

--- src/engine/Draw.cpp
+++ src/engine/Draw.cpp
@@ -4,9 +4,7 @@ void GameEngine::draw() {
   _window.clear(sf::Color::Black);
   _window.setView(_main_view);
 
-  // draw
   std::vector<std::vector<Tile>> map = _level_manager.getGrid()->getTiles();
-  std::vector<sf::Vector2i> players_positions = _level_manager.getPlayersPositions();
 
   for (int i = 0; i < MAP_HEIGHT; i++){
       for (int j = 0; j < MAP_WIDTH; j++){
