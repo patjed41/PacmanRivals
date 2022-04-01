dd8c40ce4f659c025d3d3d338b51c14f68253af2
sylwiaszunejko <sylwia.szunejko@gmail.com>
2022-04-01 09:50:08 +0200

    player movement version 1

--- src/characters/player/Pacman.cpp
+++ src/characters/player/Pacman.cpp
@@ -3,9 +3,29 @@
 
 Pacman::Pacman() : Character() {
     _sprite = sf::Sprite(TextureHolder::GetTexture("../assets/graphics/pac-man.png"));
+    _direction = STOP;
 //    _sprite.setPosition(start_tile_x, start_tile_y);
 }
 
 void Pacman::update(float dt_as_seconds) {
+    switch (_direction) {
+        case LEFT:
+            _sprite.setPosition(getPosition().left - _speed * dt_as_seconds, getPosition().top);
+            break;
+        case RIGHT:
+            _sprite.setPosition(getPosition().left + _speed * dt_as_seconds, getPosition().top);
+            break;
+        case UP:
+            _sprite.setPosition(getPosition().left, getPosition().top - _speed * dt_as_seconds);
+            break;
+        case DOWN:
+            _sprite.setPosition(getPosition().left, getPosition().top + _speed * dt_as_seconds);
+            break;
+        default:
+            return;
+    }
+}
 
+void Pacman::changeDirection(int d) {
+    _direction = static_cast<Direction>(d);
 }
--- src/engine/Input.cpp
+++ src/engine/Input.cpp
@@ -3,6 +3,7 @@
 void GameEngine::input() {
     sf::Event event;
 
+    std::vector<std::shared_ptr<Player>> players = _level_manager.getPlayers();
     while (_window.pollEvent(event)) {
         if (event.type == sf::Event::KeyPressed) {
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
@@ -12,22 +13,24 @@ void GameEngine::input() {
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return)) {
                 // pause
             }
+
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+                (*players[0].get()).turnLeft();
+            } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+                (*players[0].get()).turnRight();
+            } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+                (*players[0].get()).turnUp();
+            } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+                (*players[0].get()).turnDown();
+            }
         }
     }
 
     // handle player input
 
-    while (_window.pollEvent(event)) {
-        if(event.type == sf::Event::KeyPressed) {
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-
-            } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-
-            } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-
-            } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-
-            }
-        }
-    }
+//    while (_window.pollEvent(event)) {
+//        if(event.type == sf::Event::KeyPressed) {
+//
+//        }
+//    }
 }
