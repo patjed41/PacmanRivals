06f42145528f1fc70864e3fa380a487ce04cf4c6
Patryk Jędrzejczak <patryk.jed123@gmail.com>
2022-05-03 13:50:53 +0200

    Added visual option to check_ad_button test

--- tests/AD-button/check_ad_button.cpp
+++ tests/AD-button/check_ad_button.cpp
@@ -7,13 +7,48 @@
 int main() {
     TextureHolder textureHolder;
 
-
-
     sf::Vector2f positionA(100, 100);
     sf::Vector2f positionD(100, 500);
 
     ButtonAD buttonA(positionA, true);
-    ButtonAD buttonD(positionD, true);
+    ButtonAD buttonD(positionD, false);
+
+    sf::Clock clock;
+
+    /*sf::RenderWindow window(
+        sf::VideoMode(sf::VideoMode::getDesktopMode().width,
+                      sf::VideoMode::getDesktopMode().height),
+        "Pacman Rivals", sf::Style::Fullscreen
+    );
+
+    sf::Event event;
+
+    while (true) {
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::KeyPressed) {
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+                    window.close();
+                    exit(0);
+                }
+
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+                    buttonA.click();
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+                    buttonD.click();
+                }
+            }
+        }
+
+        float dt_as_seconds = clock.restart().asSeconds();
+        buttonA.update(dt_as_seconds);
+        buttonD.update(dt_as_seconds);
+
+        window.clear(sf::Color::Black);
+        buttonA.draw(&window);
+        buttonD.draw(&window);
+        window.display();
+    }*/
 
     err::check(!buttonA.isClicked(), 1);
     err::check(!buttonD.isClicked(), 2);
@@ -24,7 +59,6 @@ int main() {
     err::check(buttonA.isClicked(), 3);
     err::check(buttonD.isClicked(), 4);
 
-    sf::Clock clock;
     clock.restart();
     float last_time = 0;
 
