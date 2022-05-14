68ae3a4db2dfce1c89cab2464761b4e92f2c5ac8
Patryk Jędrzejczak <patryk.jed123@gmail.com>
2022-05-14 19:29:41 +0200

    Handling collisions between pacmans and power-ups

--- src/power-ups/types/firing-bullet/FiringBullet.cpp
+++ src/power-ups/types/firing-bullet/FiringBullet.cpp
@@ -1,8 +1,9 @@
 #include "FiringBullet.h"
 #include "../../../texture-holder/TextureHolder.h"
 
-FiringBullet::FiringBullet() {
+FiringBullet::FiringBullet(const sf::Vector2i &position) {
     _sprite.setTexture(TextureHolder::GetTexture("../assets/graphics/power-ups/firing-bullet.png"));
+    _sprite.setPosition(position.x * TILE_SIZE, position.y * TILE_SIZE);
 }
 
 void FiringBullet::use(std::vector<std::shared_ptr<Pacman>> pacmans, unsigned int user) {
