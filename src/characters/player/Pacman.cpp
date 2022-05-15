7d61889119ab74ec8ee76bd29a87dcbd58984884
sylwiaszunejko <sylwia.szunejko@gmail.com>
2022-05-15 11:36:25 +0200

    added SlowDow and SpeedUp classes

--- src/characters/player/Pacman.cpp
+++ src/characters/player/Pacman.cpp
@@ -90,7 +90,7 @@ void Pacman::handlePowerUpExpiry() {
             // TODO
             break;
         case SPEED_UP:
-            // TODO
+            setNormalSpeed();
             break;
         case GLUTTONY:
             // TODO
@@ -172,4 +172,12 @@ float Pacman::getPartOfPowerUpTimeLeft() const {
 
 PowerUpName Pacman::getCurrentPowerUp() const {
     return _current_power_up;
+}
+
+void Pacman::setNormalSpeed() {
+    _speed = _NORMAL_SPEED;
+}
+
+void Pacman::setSpeed(float speed) {
+    _speed = speed;
 }
---
+++ src/power-ups/types/slow_down/SlowDown.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+#include "../../PowerUp.h"
+
+class SlowDown : public PowerUp {
+
+    SlowDown() = default;
+
+    SlowDown(const sf::Vector2i &position);
+
+    void use(std::vector<std::shared_ptr<Pacman>> pacmans, unsigned int user) override;
+
+};
