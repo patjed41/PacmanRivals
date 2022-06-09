c09b3ef62507dc9e4e15ab37d4bbb73a309fa9cd
Patryk Jędrzejczak <patryk.jed123@gmail.com>
2022-06-09 17:11:04 +0200

    Finished SettingsScreen

--- src/screens/settings/SettingsScreen.h
+++ src/screens/settings/SettingsScreen.h
@@ -8,6 +8,20 @@ class SettingsScreen : Screen {
 
     sf::View _view;
 
+    sf::Font _font;
+
+    sf::Text _settings_text;
+    sf::Text _sounds_text;
+    sf::Text _music_text;
+
+    sf::Sprite _sounds_slider;
+    sf::Sprite _music_slider;
+
+    sf::Sprite _sounds_slider_handle;
+    sf::Sprite _music_slider_handle;
+
+    unsigned int _option;
+
 public:
 
     SettingsScreen() = default;
